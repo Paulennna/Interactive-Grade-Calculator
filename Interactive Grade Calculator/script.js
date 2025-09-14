aabb749895ac391345430249495efc1192aeed2e
Paulennna <mariapaula.mpgp55@gmail.com>
2025-09-14 01:27:50 -0400

    Add files via upload

---
+++ Interactive Grade Calculator/script.js
@@ -0,0 +1,36 @@
+function calculateAverage(score1, score2, score3) {
+    return (score1 + score2 + score3) / 3
+}
+
+function assingGrade(average) {
+  if (average >= 90) {
+       return "A";
+    } else if (average >= 80) {
+      return "B";
+    } else if (average >= 70) {
+      return "C";
+    } else if (average >= 60) {
+    return "D";
+    } else {
+    return "F";
+    }
+}
+
+//get user input
+
+// const score1 = parseFloat(prompt("Enter First score:"))
+// const score2 = parseFloat(prompt("Enter Second score:"))
+// const score3 = parseFloat(prompt("Enter Third score:"))
+
+const scores = [];
+for (let i = 0; i < 3; i++) {
+    const score = prompt(`Enter Score ${i + 1}:`);
+    scores.push(parseInt(score));
+    
+}
+
+const average = calculateAverage(scores[0], scores[1], scores[2])
+const grade = assingGrade(average);
+
+console.log(`Average Score: ${average}`);
+console.log(`Final Grade: ${grade}`);
